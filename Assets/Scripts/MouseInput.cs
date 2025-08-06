68c54dafb3a5834e46ab0e896c61b7db8d6e4dc4
TheCool <53025128+TheOnlyCool1@users.noreply.github.com>
2025-08-06 02:53:42 -0700

    3 space indent -> 4 space indents because they're more common.

--- Assets/Scripts/MouseInput.cs
+++ Assets/Scripts/MouseInput.cs
@@ -6,39 +6,39 @@ using UnityEngine;
 [RequireComponent(typeof(ParticleSystem))] // Used because I call GetComponent<ParticleSystem>()
 public class MouseInput : MonoBehaviour
 {
-   #region Cached Components
-   // The effects that should be played whenever the player clicks somewhere.
-   private ParticleSystem m_Particles;
-   #endregion
+    #region Cached Components
+    // The effects that should be played whenever the player clicks somewhere.
+    private ParticleSystem m_Particles;
+    #endregion
 
-   #region First Time Initialization and Set Up
-   private void Awake()
-   {
-      m_Particles = GetComponent<ParticleSystem>();
-   }
-   #endregion
+    #region First Time Initialization and Set Up
+    private void Awake()
+    {
+        m_Particles = GetComponent<ParticleSystem>();
+    }
+    #endregion
 
-   #region Main Updates
-   private void Update()
-   {
-      if (Input.GetMouseButtonDown(0))
-      {
-         Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-         wp.z = 0;
+    #region Main Updates
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            wp.z = 0;
 
-         ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams();
-         emitParams.position = wp;
-         emitParams.applyShapeToPosition = true;
-         m_Particles.Emit(emitParams, 100);
+            ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams();
+            emitParams.position = wp;
+            emitParams.applyShapeToPosition = true;
+            m_Particles.Emit(emitParams, 100);
 
-         Collider2D[] allOverlaps = Physics2D.OverlapCircleAll(wp, 1.1f);
-         Debug.Log(string.Format("Hit {0} objects", allOverlaps.Length));
-         for (int i = 0; i < allOverlaps.Length; i++)
-         {
-            GameObject target = allOverlaps[i].gameObject;
-            //Do your thing here.
-         }
-      }
-   }
-   #endregion
+            Collider2D[] allOverlaps = Physics2D.OverlapCircleAll(wp, 1.1f);
+            Debug.Log(string.Format("Hit {0} objects", allOverlaps.Length));
+            for (int i = 0; i < allOverlaps.Length; i++)
+            {
+                GameObject target = allOverlaps[i].gameObject;
+                // TODO: Do your thing here.
+            }
+        }
+    }
+    #endregion
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class SpawnManager : MonoBehaviour
 {
-   #region Editor Variables
-   [SerializeField]
-   [Tooltip("The different types of enemies that should be spawned and their corresponding spawn information.")]
-   private EnemySpawnInfo[] m_EnemyTypes;
-   #endregion
+    #region Editor Variables
+    [SerializeField]
+    [Tooltip("The different types of enemies that should be spawned and their corresponding spawn information.")]
+    private EnemySpawnInfo[] m_EnemyTypes;
+    #endregion
 
-   #region Non-Editor Variables
-   /* A timer for each enemy that should spawn an enemy of the corresponding
+    #region Non-Editor Variables
+    /* A timer for each enemy that should spawn an enemy of the corresponding
     * type when it reaches 0. Additionally, upon reaching 0, the value of the
     * timer should be reset to the appropriate value based on the enemy's spawn
     * rate. Therefore, we will have infinite spawning of the enemies.
@@ -21,61 +21,62 @@ public class SpawnManager : MonoBehaviour
     * - Implement the spawning using a coroutine instead of this using this way
     * - Make the spawn rate ramp up (may require creating a mutator in the EnemySpawnInfo struct)
     */
-   private float[] m_EnemySpawnTimers;
-   #endregion
+    private float[] m_EnemySpawnTimers;
+    #endregion
 
-   #region First Time Initialization and Set Up
-   private void Awake()
-   {
-      // Initialize the spawn timers using the FirstSpawnTime variable
-   }
-   #endregion
+    #region First Time Initialization and Set Up
+    private void Awake()
+    {
+        // Initialize the spawn timers using the FirstSpawnTime variable
+    }
+    #endregion
 
-   #region Main Updates
-   private void Update()
-   {
-      // You may want to use either a foreach or for loop (for scalability)
-      // Check if its time to spawn a particular enemy
-      // If it is, just spawn the enemy using Instantiate(m_EnemyTypes[i].EnemyPrefab)
-         // Make sure to reset the timer back to the appropriate value based on SpawnRate
-      // Else, decrease the timer using Time.deltaTime
-   }
-   #endregion
+    #region Main Updates
+    private void Update()
+    {
+        // You may want to use either a foreach or for loop (for scalability)
+        // Check if its time to spawn a particular enemy
+        // If it is, just spawn the enemy using Instantiate(m_EnemyTypes[i].EnemyPrefab)
+            // Make sure to reset the timer back to the appropriate value based on SpawnRate
+        // Else, decrease the timer using Time.deltaTime
+    }
+    #endregion
 }
 
 [System.Serializable]
 public struct EnemySpawnInfo
 {
-   #region Editor Variables
-   [SerializeField]
-   [Tooltip("The enemy prefab to spawn. This is what will be instantiated each time.")]
-   private GameObject m_EnemyPrefab;
+    #region Editor Variables
+    [SerializeField]
+    [Tooltip("The enemy prefab to spawn. This is what will be instantiated each time.")]
+    private GameObject m_EnemyPrefab;
 
-   [SerializeField]
-   [Tooltip("The time we should wait before the first enemy is spawned.")]
-   private float m_FirstSpawnTime;
+    [SerializeField]
+    [Tooltip("The time we should wait before the first enemy is spawned.")]
+    private float m_FirstSpawnTime;
 
-   [SerializeField]
-   [Range(0, 100)]
-   [Tooltip("How many enemies should spawn per second.")]
-   private float m_SpawnRate;
-   #endregion
+    [SerializeField]
+    [Range(0, 100)]
+    [Tooltip("How many enemies should spawn per second.")]
+    private float m_SpawnRate;
 
-   #region Accessors and Mutators
-   public GameObject EnemyPrefab
-   {
-      get { return m_EnemyPrefab; }
-   }
+    #endregion
 
-   public float FirstSpawnTime
-   {
-      get { return m_FirstSpawnTime; }
-   }
+    #region Accessors and Mutators
+    public GameObject EnemyPrefab
+    {
+        get { return m_EnemyPrefab; }
+    }
 
-   // Doing (1 / SpawnRate) might be more useful than directly using SpawnRate
-   public float SpawnRate
-   {
-      get { return m_SpawnRate; }
-   }
-   #endregion
+    public float FirstSpawnTime
+    {
+        get { return m_FirstSpawnTime; }
+    }
+
+    // Doing (1 / SpawnRate) might be more useful than directly using SpawnRate
+    public float SpawnRate
+    {
+        get { return m_SpawnRate; }
+    }
+    #endregion
 }
